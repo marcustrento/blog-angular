b092ba0a913dbb4846d647428b121833d68db141
Marcus Trento <marcusvinicius_ct@hotmail.com>
2023-04-18 19:40:17 -0300

    content created and readme edited

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AngularBlog
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.2.
+
+
+# ToDo
+
+Still want to improve responsivity.
+Pick a real database to use.
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Mussum Ipsum Mussum",
+        "description": "Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé, cursus quis, vehicula ac nisi.Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.Atirei o pau no gatis, per gatis num morreus.Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.",
+        "imageCover": "https://placehold.co/350x250"
+    },
+    {
+        "id": "2",
+        "title": "Mussum Ipsum",
+        "description": "Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.Pra lá , depois divoltis porris, paradis.Mé faiz elementum girarzis, nisi eros vermeio.Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.",
+        "imageCover": "https://placehold.co/350x210"
+    },
+    {
+        "id": "3",
+        "title": "Ipsum Mussum",
+        "description": "Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé, cursus quis, vehicula ac nisi.Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.Atirei o pau no gatis, per gatis num morreus.Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus.",
+        "imageCover": "https://placehold.co/350x230"
+    },
+    {
+        "id": "4",
+        "title": "Ipsum Mussum Ipsum",
+        "description": "Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.Pra lá , depois divoltis porris, paradis.Mé faiz elementum girarzis, nisi eros vermeio.Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.",
+        "imageCover": "https://placehold.co/350x250"
+    }
+]
